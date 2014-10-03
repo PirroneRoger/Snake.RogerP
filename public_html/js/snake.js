275281ea2d6b1ba708c4e48b58716ff816fe332f
Roger Pirrone <r.j.pirrone.4@gmail.com>
2014-10-03 11:27:07 -0700

    Finished milestones up to choosing a background color.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,7 +5,14 @@ var screenWidth;
 var screenHeight;
 
 function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
     
+    screenWidth = window.inner.Width;
+    screenHeight = window.inner.Height;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
@@ -13,5 +20,5 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    
+    context.fillStyle = "rbg(77, 77, 77)";
 }
