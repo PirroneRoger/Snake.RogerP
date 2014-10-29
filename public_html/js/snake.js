f0e410bc9bd071f7f040fdd4280cae4766889858
Roger Pirrone <r.j.pirrone.4@gmail.com>
2014-10-29 08:16:03 -0700

    Finished all videos

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -18,6 +18,7 @@ var gameState;
 var gameOverMenu;
 var restartButton;
 var playHUD;
+var scoreboard;
 
 /* ---------------------------------------------------------------------------- 
  * Executing Game Code
@@ -53,12 +54,14 @@ function gameInitialize() {
     restartButton.addEventListener("click", gameRestart);
     
     playHUD = document.getElementById("playHUD");
+    scoreboard = document.getElementById("scoreboard");
     
     setState("PLAY");
 }
 
 function gameLoop() {
     gameDraw();
+    drawScoreboard();
     if (gameState == "PLAY") {
         snakeUpdate();
         snakeDraw();
@@ -86,7 +89,7 @@ function gameRestart() {
 function snakeInitialize() {
     snake = [];
     snakeLength = 2;
-    snakeSize = 20;
+    snakeSize =15;
     snakeDirection = "down";
     
     for(var index = snakeLength - 1; index >= 0; index--) {
@@ -249,4 +252,8 @@ function showMenu(state) {
 function centerMenuPosition(menu) {
     menu.style.top = (screenHeight / 2) - (menu.offsetHeight / 2) + "px";
     menu.style.left = (screenWidth / 2) - (menu.offsetWidth / 2)+ "px";
+}
+
+function drawScoreboard() {
+    scoreboard.innerHTML = "Length of your Snake: " + snakeLength;
 }
